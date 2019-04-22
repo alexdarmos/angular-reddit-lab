ce0bc148ccfe3d29decca7ab356f74021269b78d
Alex Darmos <alex.darmos@gmail.com>
2019-04-22 18:18:19 -0400

    final updates

--- components/redditFeed.js
+++ components/redditFeed.js
@@ -1,4 +1,4 @@
-function RedditFeed(RedditService) {
+function RedditFeed(RedditService, $q) {
     const $ctrl = this;
     //list of reddit posts to display
     $ctrl.feed = [];
@@ -8,27 +8,50 @@ function RedditFeed(RedditService) {
     *and set $ctrl.feed to be the results
     */
     $ctrl.fetchAwwSubreddit = () => {
-       
-        //call service, then set our data
+        return $q(function(resolve, reject) {
+        //call service, then set our data                                                           
         RedditService.fetchAwwSubreddit()
-        .then((data) => {
+        .then((response) => {
             //do stuff with data
-            console.log(data);
-        }).catch((e) => {
-            console.log(`error: ${e}`)
+            console.log(response);
+            // feed = [data.data.data.children];
+            let children = response.data.data.children;
+            console.log(children[0].data.title);
+
+            children.forEach(function(child, index) {
+
+            let childObj = {
+                title: child.data.title,
+                img: child.data.thumbnail,
+                permalink: child.data.permalink
+            }
+            if (index < 10) {
+                    $ctrl.feed.push(childObj);
+            }
+
+
+
+        });
+            
+
+        }).then(() => {
+            console.log(`Success: ${resolve}`);
         })
 
-
-        
+    })
+ 
     }
-
     $ctrl.fetchAwwSubreddit();
+
   }
   
   angular.module('RedditApp').component('redditFeed', {
     template: `
-    <div ng-repeat="post in $ctrl.feed">
-        <h2>{{post.title}}</h2>
+    
+    <div id="container" ng-repeat="post in $ctrl.feed">
+        <h2 id="title">{{post.title}}</h2>
+            <img id="image" src="{{post.img}}"/>
+            <a id="reddit-link" href="https://reddit.com{{post.permalink}}" target="_blank"> See more here!</a>
     </div>
     `,
     controller: RedditFeed
